110513b72cb735b2d5ffbaecbbac98e730b0661c
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-10-28 13:33:13 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int x = 500;                 // Начальный счет
+        int y = 999;               // Сумма пополнения
+        int bonus = y / 100;       // Количество бонусов
+        int finaL;                // Итоговый баланс
+
+        if (y >= 1000) {
+            finaL = x + y + bonus;
+            System.out.println(("Итоговый счет ") + finaL);
+            System.out.println(("Бонусов ") + bonus);
+        } else {
+            finaL = x + y;
+            System.out.println(("Итоговый счёт ") + finaL);
+            System.out.println("Бонусов нет");
+        }
+
+
+    }
 }
